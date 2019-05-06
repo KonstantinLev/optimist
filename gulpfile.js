b6c4b1749f0e573b50afc4ca0c3874c7cc6bce9c
Levachin Konstantin <k.levachin@newcontact.su>
2019-05-06 15:37:27 +0300

    optimized gulpfile (add configure object)

--- gulpfile.js
+++ gulpfile.js
@@ -19,26 +19,80 @@ var ftp          = require('vinyl-ftp');
 var del          = require('del');
 
 var syntax       = 'sass'; // sass or scss;
+var paths = {
+    views: {
+        src: [
+            'app/*.html',
+            'app/*.php',
+            'app/.htaccess'
+        ],
+        dest: 'dest',
+        watch: 'app/*.php'
+    },
+    styles: {
+        src: 'app/css',
+        minName: 'script.min.css',
+        dest: 'dest/css',
+        watch: 'app/' + syntax + '/**/*.' + syntax
+    },
+    scripts: {
+        src: 'app/js',
+        minName: 'script.min.js',
+        dest: 'dest/js',
+        watch: [
+            'app/libs/**/*.js', //todo add manualy?
+            'app/js/main.js'
+        ]
+    },
+    images: {
+        src: 'app/img/**/*',
+        dest: 'dest/img',
+        watch: 'app/img/**/*'
+    },
+    fonts: {
+        src: 'app/fonts/**/*',
+        dest: 'dest/fonts',
+        watch: 'app/fonts/**/*'
+    },
+    configs: {
+        php: {
+            base: 'app',
+            port: 8010,
+            keepalive: true
+        },
+        browserSync: {
+            proxy: '127.0.0.1:8010',
+            port: 8080,
+            open: true,
+            notify: false,
+            //tunnel: true //for customer demonstration =)
+        },
+        ftp: {
+            host:      'host',
+            user:      'user',
+            password:  'password',
+            parallel:  10,
+            log: gutil.log
+        }
+    }
+};
 
 //minify css/js
 gulp.task('minify-js', function() {
-    return gulp.src([
-        'app/libs/jquery/dist/jquery.min.js',
-        'app/js/main.js', // всегда в конце
-    ])
-        .pipe(concat('script.min.js'))
+    return gulp.src(paths.scripts.watch)
+        .pipe(concat(paths.scripts.minName))
         .pipe(uglify())
-        .pipe(gulp.dest('app/js'))
+        .pipe(gulp.dest(paths.scripts.src))
         .pipe(browserSync.reload({stream: true}));
 });
 gulp.task('minify-styles', function() {
-    return gulp.src('app/' + syntax + '/**/*.' + syntax)
+    return gulp.src(paths.styles.watch)
         .pipe(sass({outputStyle: 'expand'}).on("error", notify.onError()))
         .pipe(rename({suffix: '.min', prefix : ''}))
         .pipe(autoprefixer(['last 25 versions']))
-        .pipe(concat('script.min.css'))
+        .pipe(concat(paths.styles.minName))
         .pipe(cleanCSS())
-        .pipe(gulp.dest('app/css'))
+        .pipe(gulp.dest(paths.styles.src))
         .pipe(browserSync.reload({stream: true}));
 
 });
@@ -46,104 +100,67 @@ gulp.task('minify', gulp.parallel('minify-js', 'minify-styles'));
 
 // compress image
 gulp.task('compress-img', function (cb) {
-    gulp.src('app/img/**/*')
+    gulp.src(paths.images.src)
         //.pipe(gulp.dest('dest/img')) //Копируем изображения заранее, imagemin может "забагать")
-        // .pipe(cache(imagemin([
-        //     imagemin.gifsicle({interlaced: true}),
-
+            //todo
+        // [
+        // imagemin.gifsicle({interlaced: true}),
+        //     imageminJpgtr({progressive: true}),
         //     imageminPng({quality: '80'}),
         //     imagemin.svgo({plugins: [{removeViewBox: true}]})
-        // ])))
-        .pipe(cache(imagemin([
-            imagemin.gifsicle({interlaced: true}),
-            imageminJpgtr({progressive: true}),
-            imageminPng({quality: '80'}),
-            imagemin.svgo({plugins: [{removeViewBox: true}]})
-            ], {verbose: true}
-        )))
-        .pipe(gulp.dest('dest/img'));
+        // ]
+        .pipe(cache(imagemin({verbose: true})))
+        .pipe(gulp.dest(paths.images.dest));
     cb();
 });
 
-
 gulp.task('remove-dest', function(cb) {
-    del.sync('dest');
+    del.sync(paths.views.dest);
     cb();
 });
 
 gulp.task('create-dest', function(cb) {
-    gulp.src([
-        'app/*.html',
-        'app/*.php',
-        'app/.htaccess',
-    ], {allowEmpty: true}).pipe(gulp.dest('dest'));
-
-    gulp.src([
-        'app/css/script.min.css',
-    ]).pipe(gulp.dest('dest/css'));
-
-    gulp.src([
-        'app/js/script.min.js',
-    ]).pipe(gulp.dest('dest/js'));
-
-    gulp.src([
-        'app/fonts/**/*',
-    ]).pipe(gulp.dest('dest/fonts'));
+    gulp.src(paths.views.src, {allowEmpty: true}).pipe(gulp.dest(paths.views.dest));
+    gulp.src(paths.styles.src + '/' + paths.styles.minName).pipe(gulp.dest(paths.styles.dest));
+    gulp.src(paths.scripts.src + '/' + paths.scripts.minName).pipe(gulp.dest(paths.scripts.dest));
+    gulp.src(paths.fonts.src).pipe(gulp.dest(paths.fonts.dest));
     cb();
 });
 
 gulp.task('build', gulp.series('remove-dest', 'compress-img', 'minify', 'create-dest'));
 
-
 //deploy files on your server
 gulp.task('deploy', function() {
-
-    var conn = ftp.create({
-        host:      'host',
-        user:      'user',
-        password:  'password',
-        parallel:  10,
-        log: gutil.log
-    });
-
+    var conn = ftp.create(paths.configs.ftp);
     var globs = [
-        'src/**',
-        'src/.htaccess',
+        'dest/**',
+        'dest/.htaccess',
     ];
     return gulp.src(globs, {buffer: false})
         .pipe(conn.dest('path/to/folder/on/server'));
-
 });
-
 
 gulp.task('clearcache', function () { return cache.clearAll(); });
 
 gulp.task('php-server', function() {
-    php.server({ base: 'app', port: 8010, keepalive: true});
+    php.server(paths.configs.php);
 });
 
-
 gulp.task('bs', function() {
-    browserSync({
-        proxy: '127.0.0.1:8010',
-        port: 8080,
-        open: true,
-        notify: false
-    });
+    browserSync(paths.configs.browserSync);
 });
 
 gulp.task('browser-sync', gulp.parallel('php-server', 'bs'));
 
 gulp.task('code', function() {
-    return gulp.src('app/*.php')
+    return gulp.src(paths.views.watch)
         .pipe(browserSync.reload({ stream: true }))
 });
 
-
 gulp.task('watch', function() {
-    gulp.watch('app/' + syntax + '/**/*.' + syntax, gulp.series('minify-styles'));
-    gulp.watch(['app/libs/**/*.js', 'app/js/main.js'], gulp.series('minify-js'));
-    gulp.watch('app/*.php', gulp.series('code'));
+    gulp.watch(paths.styles.watch, gulp.series('minify-styles'));
+    gulp.watch(paths.scripts.watch, gulp.series('minify-js'));
+    gulp.watch(paths.views.watch, gulp.series('code'));
 });
 
-gulp.task('default', gulp.parallel('minify', 'browser-sync', 'watch'));
+gulp.task('default', gulp.parallel('browser-sync', 'watch'));
